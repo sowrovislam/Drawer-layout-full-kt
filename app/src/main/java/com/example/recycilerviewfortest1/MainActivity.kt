4068469a739e0544feafb4e9a60831b9289a222c
sowro <sowrovnil59@gmail.com>
2024-10-31 16:02:42 +0600

    the code  is full  darawer layout  for oll data in layout ol detels

--- app/src/main/java/com/example/recycilerviewfortest1/MainActivity.kt
+++ app/src/main/java/com/example/recycilerviewfortest1/MainActivity.kt
@@ -10,21 +10,24 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.recycilerviewfortest1.databinding.ActivityMainBinding
+import com.example.recycilerviewfortest1.databinding.NavHeaderBinding
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val headerView = binding.navView.getHeaderView(0)
+        val headerBinding = NavHeaderBinding.bind(headerView)
 
-//        Drawer Layout
+
+          //  start      Drawer Layout
 
         setSupportActionBar(binding.toolbar)
         navController = findNavController(R.id.nav_host_fragment)
@@ -34,23 +37,43 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfigurationdata()
 
+            //drawer layout  END
+
+
+        // menu bar item start
 
         binding.navView.setNavigationItemSelectedListener { menu->
+
 
 
             menu.isChecked=true
 
             when(menu.itemId){
 
-                R.id.blankFragment3->   Toast.makeText(this, "ami toake ", Toast.LENGTH_SHORT).show()
 
+                R.id.blankFragment3->  {
+                    Toast.makeText(this, "ami toake ", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.closeDrawers()
 
+                }
+                R.id.blankFragment->  {
+                    Toast.makeText(this, "ami toake ", Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.closeDrawers()
 
+                }
             }
-
-
             return@setNavigationItemSelectedListener true
         }
+
+//        // menu bar  END
+//
+//
+
+
+
+
+
+
 
 
 
